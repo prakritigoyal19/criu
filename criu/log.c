6d09da11ef2e74dc1472b63be24cd7eae4260286
prakritigoyal19 <prakritigoyal19@gmail.com>
2020-08-19 23:49:29 +0530

    Wire flog into criu
    This commit adds flog to criu build and uses it whenever binary logging
    is enabled.
    
    Signed-off-by: prakritigoyal19 <prakritigoyal19@gmail.com>

--- criu/log.c
+++ criu/log.c
@@ -34,7 +34,6 @@
 #define EARLY_LOG_BUF_LEN	1024
 
 static unsigned int current_loglevel = DEFAULT_LOGLEVEL;
-static void vprint_on_level(unsigned int, const char *, va_list);
 
 static char buffer[LOG_BUF_LEN];
 static char buf_off = 0;
@@ -350,8 +349,7 @@ static void early_vprint(const char *format, unsigned int loglevel, va_list para
 	hdr->len = log_size;
 	early_log_buf_off += log_size;
 }
-
-static void vprint_on_level(unsigned int loglevel, const char *format, va_list params)
+void vprint_on_level(unsigned int loglevel, const char *format, va_list params)
 {
 	int fd, size, ret, off = 0;
 	int _errno = errno;
